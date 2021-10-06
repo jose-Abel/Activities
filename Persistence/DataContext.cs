564f0f5719e9ad100c40c1bd627d281e44ee78c5
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-05 20:02:40 -0400

    Create the first migration

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,7 +11,7 @@ namespace Persistence
     {
         public DataContext( DbContextOptions options) : base(options)
         {
-
+            
         }
 
         public DbSet<Activity> Activities { get; set; }
