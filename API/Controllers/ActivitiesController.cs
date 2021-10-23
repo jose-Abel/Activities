883bc030310b14cba7dd56d175a613d921966536
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-23 15:58:37 -0400

    Implement the Details class with the Mediator pattern to get one activity based on the Id

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-      return Ok();
+      return await Mediator.Send(new Details.Query { Id = id });
     }
   }
 }
