0fea8b5432b976da1cb509fc184207b908879870
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-23 16:58:58 -0400

    Create the Edit class in the Application solution with the Mediator pattern and call the mediator to send an instantiation of the Edit class as a response for the HttpPut route

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -28,5 +28,13 @@ namespace API.Controllers
     {
       return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+      activity.Id = id;
+
+      return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+    }
   }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,36 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+  public class Edit
+  {
+    public class Command : IRequest
+    {
+      public Activity Activity { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+      private readonly DataContext _context;
+      public Handler(DataContext context)
+      {
+        _context = context;
+      }
+
+      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      {
+        var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+        activity.Title = request.Activity.Title ??= activity.Title;
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+      }
+    }
+  }
+}
