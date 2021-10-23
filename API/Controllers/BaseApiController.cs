bce8b61b5a7f17a9640bab40f0fbd854653c13b1
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-23 15:52:44 -0400

    Make the ActivitiesController thinner by removing the logic of injecting the IMediator to the BaseApiController which all controllers inherits from

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BaseApiController : ControllerBase
-    {
+  [ApiController]
+  [Route("api/[controller]")]
+  public class BaseApiController : ControllerBase
+  {
+    private IMediator _mediator;
 
-    }
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+  }
 }
