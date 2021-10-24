c17c434d747b2dcc71c9a34460b3d932a2926b49
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-24 16:20:42 -0400

    House cleaning on the services from the Startup class by creating an ApplicationServiceExtensions class

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 using Application.Activities;
 using Application.Core;
 using AutoMapper;
@@ -33,27 +34,9 @@ namespace API
     {
 
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
 
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
+      services.AddApplicationServices(_config);
 
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-        {
-          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-        });
-      });
-
-      services.AddMediatR(typeof(List.Handler).Assembly);
-
-      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
