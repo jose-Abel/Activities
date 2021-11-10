5db122c0e535f7c7cfd67331ece263f9600980c2
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-10 10:03:02 -0400

    Add the NavLinks to Route to the HomePage, Activities and ActivityForm

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import { Route } from 'react-router-dom';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Container style={{ marginTop: '7em'}}>
 
         <Route exact path="/" component={HomePage} />
-        <Route path="/activities" component={ActivityDashboard} />
+        <Route exact path="/activities" component={ActivityDashboard} />
+        <Route path="/activities/:id" component={ActivityDetails} />
         <Route path="/createActivity" component={ActivityForm} />
 
       </Container>
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from "semantic-ui-react";
 import { useStore } from '../stores/store';
 
@@ -8,18 +9,18 @@ export default function NavBar() {
   return (
     <Menu inverted fixed="top">
       <Container>
-          <Menu.Item header>
+          <Menu.Item as={NavLink} exact to='/' header>
             <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }}/>
             Reactivities
           </Menu.Item>
       
-          <Menu.Item name="Activities"/>
+          <Menu.Item as={NavLink} to='/activities' name="Activities"/>
 
           <Menu.Item>
             <Button 
               positive 
               content="Create Activity"
-              onClick={() => activityStore.openForm()}
+              as={NavLink} to='/createActivity'
             />
           </Menu.Item>
       </Container>
