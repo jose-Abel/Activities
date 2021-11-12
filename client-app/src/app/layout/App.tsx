b85577191f79fac3d24acbbf35fd2a78453457a7
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-12 09:59:19 -0400

    Use the useLocation hook to pass a key to the ActivityForm Route so it re-renders the component when the key changes to change the form base on the id. Use the useHistory hook to redirect to the right page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,21 +5,33 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em'}}>
+      <Route exact path="/" component={HomePage} />
 
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/activities" component={ActivityDashboard} />
-        <Route path="/activities/:id" component={ActivityDetails} />
-        <Route path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+      <Route 
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            
+            <Container style={{ marginTop: '7em'}}>
+        
+            <Route exact path="/activities" component={ActivityDashboard} />
+            
+            <Route path="/activities/:id" component={ActivityDetails} />
+            
+            <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
 
       </Container>
+          </>
+        )}
+      />
     </>
   );
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import {Form, Segment, Button } from "semantic-ui-react";
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import { v4 as uuid } from 'uuid';
 
 export default observer(function AcvitityForm() {
   const {activityStore} = useStore();
+  const history = useHistory();
 
   const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
 
@@ -29,7 +31,16 @@ export default observer(function AcvitityForm() {
   }, [id, loadActivity]);
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity);
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      }
+      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+    }
+    else {
+      updateActivity(activity).then(() => history.push(`/activities/${activity.id}`))
+    }
   }
 
   function handleInputChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
