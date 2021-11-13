96619492e2bff73c625af2a0388cb8e77b3d7ec5
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-13 14:19:34 -0400

    Create the get groupedActivities method in the activityStore

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,31 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Item, Segment } from 'semantic-ui-react';
+import React, { Fragment } from 'react';
+import { Header, Item, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
-import ActivityListItem from "./ActivityListItem"l
+import ActivityListItem from "./ActivityListItem";
 
 export default observer(function ActivityList() {
   const {activityStore} = useStore();
 
-  const {activitiesByDate} = activityStore;
+  const {groupedActivities} = activityStore;
 
   return(
-    <Segment>
-      <Item.Group divided>
-        {activitiesByDate.map(activity => (
-          <ActivityListItem key={activity.id} activity={activity} />
-        ))}
-      </Item.Group>
+    <>
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
 
-    </Segment>
+          <Segment>
+            <Item.Group divided>
+              {activities.map(activity => (
+                <ActivityListItem key={activity.id} activity={activity} />
+              ))}
+            </Item.Group>
+          </Segment>
+        </Fragment>
+      ))}
+    </>
   )
 });
