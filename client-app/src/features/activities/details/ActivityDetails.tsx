a3347d4c5a8ee203537999bce08b1928793d7a93
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-15 15:17:40 -0400

    Populating the activity detailed components

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,8 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import { Button, Card, Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetailedHeader from './ActivityDetailedHeader';
@@ -26,8 +25,8 @@ export default observer(function ActivityDetails() {
   return (
    <Grid>
      <Grid.Column width={10}>
-       <ActivityDetailedHeader />
-       <ActivityDetailedInfo />
+       <ActivityDetailedHeader activity={activity} />
+       <ActivityDetailedInfo activity={activity} />
        <ActivityDetailedChat />
      </Grid.Column>
      
